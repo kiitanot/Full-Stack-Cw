a443879df772e7923783e16df9f34df028bfb97a
Kitan <ko460@live.mdx.ac.uk>
2024-11-26 14:57:47 +0000

    Initial commit

---
+++ products.js
@@ -0,0 +1,115 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "French",
+        "description": "Learn french with our course for begginers",
+        "price": 6.79,
+        "image": "images/image01.png",
+        "availableInventory": 10,
+        "location": "Birmingham",
+        "rating": 2
+    },
+
+    {
+        "id": 1002,
+        "title": "Computer science",
+        "description": "Introduction to computer science",
+        "price": 7.89,
+        "image": "images/image02.png",
+        "availableInventory": 7,
+        "location": "Oxford",
+        "rating": 3
+    },
+
+    {
+        "id": 1003,
+        "title": "Maths",
+        "description": "Master basic maths concepts with our engaging course",
+        "price": 14.99,
+        "image": "images/image03.png",
+        "availableInventory": 11,
+        "location": "Edinburgh",
+       "rating": 4
+    },
+
+    {
+        "id": 1004,
+        "title": "Organic Chemistry",
+        "description": "Learn organic chemistry with practice problems",
+        "price": 11.59,
+        "image": "images/image04.png",
+        "availableInventory": 5,
+        "location": "York",
+        "rating": 5
+    },
+
+    {
+        "id": 1005,
+        "title": "Coding",
+        "description": "Learn to code step-by-step with our coding course",
+        "price": 7.99,
+        "image": "images/image05.png",
+        "availableInventory": 10,
+        "location": "Luton",
+       "rating": 4
+    },
+
+    {
+        "id": 1006,
+        "title": "Introductory Biology",
+        "description": "Life around us is simply amazing, learn how it works",
+        "price": 8.79,
+        "image": "images/image06.png",
+        "availableInventory": 10,
+        "location": "London",
+        "rating": 2
+    },
+
+    {
+        "id": 1007,
+        "title": "Geography",
+       "description": "Explore the complexities of geography",
+       "price": 5.89,
+        "image": "images/image07.png",
+       "availableInventory": 7,
+       "location": "London",
+       "rating": 3
+    },
+
+    {
+        "id": 1008,
+        "title": "Physics",
+        "description": "Explore the world of physics with our beginner course,",
+        "price": 12.99,
+        "image": "images/image08.png",
+        "availableInventory": 16,
+        "location": "London",
+        "rating": 4
+    },
+
+    {
+        "id": 1009,
+        "title": "Art Theory",
+        "description": "Drawing isn't only a talent, it's a skill anyone can learn",
+        "price": 11.45,
+        "image": "images/image09.png",
+        "availableInventory": 5,
+        "location": "Plymouth",
+       "rating": 5
+    },
+
+    {
+        "id": 1010,
+        "title": "Korean",
+        "description": "Build up your korean skills with our intermediate course",
+        "price": 7.99,
+        "image": "images/image10.png",
+        "availableInventory": 10,
+        "location": "York",
+       "rating": 4
+    }
+
+]
+
+
+
